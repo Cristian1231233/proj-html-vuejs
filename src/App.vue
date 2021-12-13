34afb92384677f4844d9dc2a5840eb78030e77e8
Cristian1231233 <campagnacristian22@gmail.com>
2021-12-13 11:15:43 +0100

    struttura base

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-    
-
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+   import Header from './components/Header.vue'
+   import Main from './components/Main.vue'
+   import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    
+     Header,
+     Main,
+     Footer
   }
 }
 </script>
